2ef5cb040c4e5d06c6463dd5b20124c6d5a7405f
Abdul-Razzaq-40 <abdulrazzakm40@gmail.com>
2022-11-23 10:33:46 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Content from './Components/Content';
+
+const App=() =>{
+
+  return (
+    <div className="App">
+      <h1>Inventory Record</h1>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
